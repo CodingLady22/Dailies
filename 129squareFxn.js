a9a1197b0367cbf95e459fb5e8cee74c985db243
Maye Jesuorobo <nayawes@yahoo.com>
2022-04-16 00:22:51 +0200

    multiply the parameter by itself

---
+++ 129squareFxn.js
@@ -0,0 +1,10 @@
+/* 
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+function square (num) {
+
+  return num * num
+  
+}
