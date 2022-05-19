31b3ac20e3f772a8ee8a522468c3a7b108978bbd
Maye Jesuorobo <nayawes@yahoo.com>
2022-05-20 00:26:04 +0200

    using math.pow, raise the power of the parameter by 2

---
+++ 129aSquareFxn.js
@@ -0,0 +1,11 @@
+/*
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+function square(num){
+
+ return Math.pow(num, 2);
+
+}
+console.log(square(5));
