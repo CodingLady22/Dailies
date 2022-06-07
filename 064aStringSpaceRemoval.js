7f68f95d5f5752f43321d52968a4d12f42681f38
Maye Jesuorobo <nayawes@yahoo.com>
2022-06-07 19:33:21 +0200

    split and join

---
+++ 064aStringSpaceRemoval.js
@@ -0,0 +1,10 @@
+/* 64 Pushed 09/Oct/2022 (064.js)
+Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+  
+  return x.split(' ').join('')
+
+}
