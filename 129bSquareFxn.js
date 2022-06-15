9e10741b2fa0cdb99cd56ae2402d45542a7fb73b
Maye Jesuorobo <nayawes@yahoo.com>
2022-06-16 00:28:35 +0200

    using math.sqrt, raise the power of the parameter by 4

---
+++ 129bSquareFxn.js
@@ -0,0 +1,12 @@
+/*
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+const square = (x) => {
+
+return Math.sqrt(x**4)
+
+}
+
+console.log(square(5));
