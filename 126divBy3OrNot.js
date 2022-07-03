36d0b910aaebe9d7ef1422a290f4083eb5b0e6c2
Maye Jesuorobo <nayawes@yahoo.com>
2022-07-03 22:31:06 +0200

    for loop then turn each str to a num then % 3

---
+++ 126divBy3OrNot.js
@@ -0,0 +1,26 @@
+/*
+7kyu By 3, or not by 3? That is the question . . .
+
+A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+Example:
+
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false
+Try to avoid using the % (modulo) operator.
+*/
+
+function divisibleByThree(str){
+  
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) 
+        sum += Number(str[i])
+ 
+    return (sum % 3 == 0);
+
+}
