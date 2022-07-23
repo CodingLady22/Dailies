eacf467b8affd21fb0501b56d3ec544847834c56
Maye Jesuorobo <nayawes@yahoo.com>
2022-07-23 23:09:50 +0200

    mathMaxMin

--- 026calcAverage.js
+++ 026calcAverage.js
@@ -4,8 +4,13 @@ Write a function which calculates the average of the numbers in a given list.
 Note: Empty arrays should return 0.
 */
 function find_average(array) {
+  return (Math.min(...array) + Math.max(...array)) / 2;
+}
+
+/*function find_average(array) {
 
   let sum = array.reduce((sum,acc) => sum + acc, 0)
   return array.length === 0 ? 0 : sum/array.length
 
 }
+*/
