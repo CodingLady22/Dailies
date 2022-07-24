2d5ade77d285930116bd0e23070049b7763c4775
Maye Jesuorobo <nayawes@yahoo.com>
2022-07-24 21:26:44 +0200

    BMI

--- 025bodyMI.js
+++ 025bodyMI.js
@@ -9,3 +9,16 @@ if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"
 */
+function bmi(w, h) {
+  let b = w / h **2
+  if(b <= 18.5) {
+    return "Underweight"
+  } else if(b <= 25.0) {
+    return "Normal"
+  } else if(b <= 30.0) {
+    return "Overweight"
+  } else if(b > 30.0) {
+    return "Obese"
+  } 
+  return b
+}
