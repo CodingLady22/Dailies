a548bc0b455e36a7293a59c72efdbbb2e00cad99
Maye Jesuorobo <nayawes@yahoo.com>
2022-08-06 19:39:05 +0200

    reduce

---
+++ 064bStringSpaceRemoval.js
@@ -0,0 +1,9 @@
+/* 64 Pushed 09/Oct/2022 (064.js)
+Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+ let arr =  x.split(' ');
+ return arr.reduce((a,b)=>a+b);
+}
