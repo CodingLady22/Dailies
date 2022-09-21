b7eb63b11e54ed34fc40e6e31bf3d465b1f2499f
Maye Jesuorobo <nayawes@yahoo.com>
2022-09-22 00:47:47 +0200

    get the charAt at the index of each word with the matching index of the letter to that word

---
+++ 120bSubstrFun.js
@@ -0,0 +1,19 @@
+/*  
+Substring fun
+Complete the function that takes an array of words.
+
+You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+For example:
+
+["yoda", "best", "has"]  -->  "yes"
+  ^        ^        ^
+  n=0     n=1     n=2
+Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+*/
+
+function nthChar(words){
+
+ return words.map((str, i) => str.charAt(i)).join("");
+ 
+}
