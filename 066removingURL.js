f8a252876c78e636a9e90c9f7b501e61461d8045
Maye Jesuorobo <nayawes@yahoo.com>
2022-10-11 20:21:25 +0200

    sliced from the index of the pound if the pound is included

---
+++ 066removingURL.js
@@ -0,0 +1,18 @@
+/*
+Remove anchor from url
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/'), 'www.codewars.com/katas/')
+assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/?page=1#about'), 'www.codewars.com/katas/?page=1')
+*/
+
+function removeUrlAnchor(url){
+  
+  return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
+  
+  console.log(url)
+}
