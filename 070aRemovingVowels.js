7f152273ba941befd4354b9b1e079f1f5c3660a8
Maye Jesuorobo <nayawes@yahoo.com>
2022-10-17 17:19:26 +0200

    filter to not include vowels

---
+++ 070aRemovingVowels.js
@@ -0,0 +1,18 @@
+/* 
+Vowel remover
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+*/
+
+function shortcut (string) {
+  return string
+    .split('')
+    .filter(str => !'aeiou'.includes(str))
+    .join('')
+}
