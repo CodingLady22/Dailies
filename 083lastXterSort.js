cf7987b96033aebe78087f8edcac2bb65b7eae64
Maye Jesuorobo <nayawes@yahoo.com>
2022-11-02 18:30:54 +0100

    split and sort with charCodeAt

---
+++ 083lastXterSort.js
@@ -0,0 +1,16 @@
+/* 105 
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+*/
+
+function last(x){  
+  
+  let arr = x.split(' ')
+  .sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))  
+  
+  return arr
+
+}
