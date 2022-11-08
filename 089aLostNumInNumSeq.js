1109b94fc12c979f0af66c9590a208e9efdfe0ab
Maye Jesuorobo <nayawes@yahoo.com>
2022-11-08 21:45:52 +0100

    reduce both then subtract missing from original

---
+++ 089aLostNumInNumSeq.js
@@ -0,0 +1,21 @@
+/* 
+Lost number in number sequence
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+*/
+
+function findDeletedNumber(arr, mixArr) {
+  
+   let arrSum = arr.reduce((a,b) => a + b, 0)
+  let mixArrSum = mixArr.reduce((a,b) => a + b, 0)
+  return arrSum - mixArrSum
+  
+}
