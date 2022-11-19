e4049708c947f53c3e9be484c677396f571f2253
Maye Jesuorobo <nayawes@yahoo.com>
2022-11-19 21:55:23 +0100

    split then map elements to uppercase

---
+++ 101camelCaseBreaker.js
@@ -0,0 +1,23 @@
+/*
+Break camelCase
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+  
+ let str = string.split('').map(el => {
+   if (el === el.toUpperCase()) {
+     el = ' ' + el
+   } 
+     return el
+   
+ })
+ return str.join('')
+  
+}
