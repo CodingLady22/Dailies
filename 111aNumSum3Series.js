e4859841003d009aa843c0bf28efee649bccbe30
Maye Jesuorobo <nayawes@yahoo.com>
2022-12-06 20:34:22 +0100

    save a & b in varaibles to alternate when a > b

---
+++ 111aNumSum3Series.js
@@ -0,0 +1,32 @@
+/* 
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum( a,b ) {
+   let sum = 0, x = a, y = b;
+    if (a === b) {
+        return a
+    }
+    if (a > b) {
+        x = b, y = a;
+    }
+
+    for( let i = x; i <= y; i++) {
+        sum += i
+    }
+    return sum
+
+}
+
+console.log(getSum(1, 0))
