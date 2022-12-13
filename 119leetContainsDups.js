ae4bc1be128d96b724e9c0f334de7dc95f16f93e
Maye Jesuorobo <nayawes@yahoo.com>
2022-12-13 22:27:28 +0100

    hash map. add all numbers until a duplicate is found

---
+++ 119leetContainsDups.js
@@ -0,0 +1,41 @@
+/*
+Contains Duplicate
+  Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109 
+*/
+
+var containsDuplicate = function(nums) {
+        let numsMap = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i]
+
+        if(numsMap[num]) {
+            return true
+        } else {
+             numsMap[num] = true
+        }
+
+    }
+        return false 
+};
