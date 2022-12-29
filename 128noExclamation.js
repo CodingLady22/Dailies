9492668e5ad43910701cc7ea4ae6adf3518b8135
Maye Jesuorobo <nayawes@yahoo.com>
2022-12-29 23:06:54 +0100

    conditional, endswith and slice

---
+++ 128noExclamation.js
@@ -0,0 +1,22 @@
+/*
+Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+Description:
+
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"
+*/
+
+function remove (string) {
+  
+  return string.endsWith('!') ? string.slice(0, -1) : string
+  
+}
