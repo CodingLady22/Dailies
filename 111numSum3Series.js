db6497fc0fa7bd7216c92543788fd29cd4a0ade6
Maye Jesuorobo <nayawes@yahoo.com>
2023-03-07 22:34:01 +0100

    use a maths equation involving the math max and math min

---
+++ 111numSum3Series.js
@@ -0,0 +1,26 @@
+/* 
+Number series 3
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum( a,b ) {
+   
+let max = Math.max(a, b),
+    min = Math.min(a,b);
+   
+  return (max - min + 1) * (max + min) / 2
+
+}
+
+console.log(getSum(1, 0))
