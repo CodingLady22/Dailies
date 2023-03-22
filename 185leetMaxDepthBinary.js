a885fc295c61ae60a9d769cd7efd18016bb3d9d7
Maye Jesuorobo <nayawes@yahoo.com>
2023-03-22 21:36:47 +0100

    if there is no root, return 0, else find the max between the left and right sides then add 1 to which ever side is the max side

---
+++ 185leetMaxDepthBinary.js
@@ -0,0 +1,28 @@
+/* 
+Maximum Depth of Binary Tree
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+*/
+
+let maxDepth = function(root) {
+
+    if(!root) {
+        return 0
+    }
+
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+    
+};
