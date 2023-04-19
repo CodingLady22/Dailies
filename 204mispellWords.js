c84414a1328f5b33d35ea99d361bdb74263b75e2
Maye Jesuorobo <nayawes@yahoo.com>
2023-04-19 20:12:17 +0200

    if differences are more than 1, return false. loop, check is first parameter are in the 2nd parameter

---
+++ 204mispellWords.js
@@ -0,0 +1,33 @@
+/* 
+Mispelled word
+Create a function mispelled(word1, word2):
+
+mispelled('versed', 'xersed'); // returns true
+mispelled('versed', 'applb'); // returns false
+mispelled('versed', 'v5rsed'); // returns true
+mispelled('1versed', 'versed'); // returns true
+mispelled('versed', 'versed'); // returns true
+It checks if the word2 differs from word1 by at most one character.
+
+This can include an extra char at the end or the beginning of either of words.
+
+In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
+*/
+
+var mispelled = function(word1, word2) {
+
+    if ((word1.length - word2.length) > 1) return false;
+    if ((word2.length - word1.length) > 1) return false;
+
+    let count = 0;
+
+    for (let i = 0; i < word1.length; i++) {
+        if (!word2.includes(word1[i])) {
+            count++;
+        }
+    }
+    return count <= 1 ? true : false;
+
+}
+
+console.log(mispelled ('versed', 'versedqr'));
