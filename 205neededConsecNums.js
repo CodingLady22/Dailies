f7d83962de213ea1f7724aaa3e1ee6c19afa7c06
Maye Jesuorobo <nayawes@yahoo.com>
2023-04-20 16:49:46 +0200

    if arr length > 1, subtract the max value from the min value then subtract the difference from the length of the arr minus 1

---
+++ 205neededConsecNums.js
@@ -0,0 +1,13 @@
+/* 
+How many consecutive numbers are needed?
+Create the function consecutive(arr) that takes an array of integers and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number.
+
+For example: 
+If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
+*/
+
+function consecutive(arr) {
+//code me
+  return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) - (arr.length - 1) : 0
+}
+console.log(consecutive([4, 8, 6]));
