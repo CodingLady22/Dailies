2b9b89ce9901c0613979c027bea73b42a942b004
Maye Jesuorobo <nayawes@yahoo.com>
2023-04-25 21:12:22 +0200

    filter out the odd numbers

---
+++ 208jsArrayFilter.js
@@ -0,0 +1,14 @@
+/*
+JavaScript Array Filter
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+function getEvenNumbers(numbersArray){
+  // filter out the odd numbers
+  
+  return numbersArray.filter(el => el % 2 === 0)
+}
