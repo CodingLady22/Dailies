da40633993e3ad9293a431eece17fe437c7ba229
Maye Jesuorobo <nayawes@yahoo.com>
2023-05-27 11:25:30 +0100

    error removed

--- 114aNoFives.js
+++ 114aNoFives.js
@@ -1,4 +1,4 @@
-/* 137 Pushed 09/Dec/22 (114.js)
+/*
 Don't give me five!
 In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
 
