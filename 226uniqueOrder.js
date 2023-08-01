c9b9859ea173e8cb754f1399a791663639dfff79
Maye Jesuorobo <nayawes@yahoo.com>
2023-08-01 22:49:15 +0200

    loop, if the current item is the same as the next item, skip it

---
+++ 226uniqueOrder.js
@@ -0,0 +1,27 @@
+/* 
+Unique In Order
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+var uniqueInOrder = function(iterable){
+  
+  let newArr = []
+  
+  for(let i = 0; i < iterable.length; i++) {
+    if(iterable[i] != iterable[i + 1]){
+      newArr.push(iterable[i])
+    }
+  }
+  
+  return newArr
+}
+
+console.log(uniqueInOrder('ABBCcAD'));
+// console.log(uniqueInOrder([1,2,2,3,3]));
+// console.log(uniqueInOrder('AAAABBBCCDAABBB'));
