8a07f02f77aff39c81f18ec05d4339b8b4797dca
Maye Jesuorobo <nayawes@yahoo.com>
2023-09-12 23:31:15 +0200

    use Array.fill to fill in the quantity of the string required

---
+++ 234kataEgTwist.js
@@ -0,0 +1,9 @@
+/* 
+Kata Example Twist
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+*/
+
+var websites = Array(1000).fill("codewars")
