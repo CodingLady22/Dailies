d1e521f1fb6d293ed7112a0ef7a868a244ec8304
Maye Jesuorobo <nayawes@yahoo.com>
2023-11-20 22:47:52 +0100

    check if lengths are equal before making an array of 26 zeros. With equals, any letter from one should be crossed out by the second parameter. Confirm the whole array remains as zeros

---
+++ 251aLeetCodeAnagram.js
@@ -0,0 +1,43 @@
+/* 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+
+var isAnagram = function(s, t) {
+
+    if(s.length != t.length) {
+        return false
+    }
+
+    let values = Array(26).fill(0);
+
+    for(let i = 0; i < s.length; i++) {
+        values[s.charCodeAt(i) - 97]++
+        values[t.charCodeAt(i) - 97]--
+    }
+    
+    return values.every(letter => letter === 0)
+};
+
+console.log(isAnagram("anagram", "nagaram"));
