b83aa74949d9491b831fe7fab2ce9bead6f4059b
Maye Jesuorobo <nayawes@yahoo.com>
2024-03-18 23:07:00 +0100

    find the desired language to check if a developer with that language is registered or not

---
+++ 289codeMeetUp4.js
@@ -0,0 +1,42 @@
+/* 
+Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+Your task is to return one of the following strings:
+
+< firstName here >, < country here > of the first Python developer who has signed up; or
+There will be no Python developers if no Python developer has signed up.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+your function should return Victoria, Puerto Rico.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+
+Example 2
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+]
+*/
+
+function getFirstPython(list) {
+  // Thank you for checking out my kata :)
+
+    let firstDev = list.find(dev => dev.language === "Python")
+
+    return firstDev ? firstDev.firstName + ", " + firstDev.country : "There will be no Python developers"
+}
+
+console.log(getFirstPython([
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+]));
